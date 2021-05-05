c6a810bfe4bff47ba7c8f79820df00e270f3d61e
Ashan Perera <ashanperera.201@gmail.com>
2021-05-05 23:55:02 +0530

    finalizing the ui part.

--- src/app/global-search/global-search.component.ts
+++ src/app/global-search/global-search.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-// import { ChartType } from 'angular-google-charts';
+import { Component, OnInit } from '@angular/core';
 import { GlobalSearchService } from './global-search.service';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { switchMap } from 'rxjs/operators';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
-import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'global-search',
@@ -19,83 +16,83 @@ export class GlobalSearchComponent implements OnInit {
   @BlockUI() blockUI!: NgBlockUI;
 
 
-  barChartOptions: ChartOptions = {
-    responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: { xAxes: [{}], yAxes: [{}] },
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
+  // finalized data object.
+  mostVisited: any = {
+    barChartType: 'bar',
+    barChartLabels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    barChartData: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    ],
+    barChartPlugins: [pluginDataLabels],
+    barChartLegend: true,
+    barChartOptions: {
+      responsive: true,
+      scales: { xAxes: [{}], yAxes: [{}] },
+      plugins: {
+        datalabels: {
+          anchor: 'end',
+          align: 'end',
+        }
       }
     }
-  };
-  barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  barChartType: ChartType = 'bar';
-  barChartLegend = true;
-  barChartPlugins:any = [pluginDataLabels];
+  }
 
-  barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  ];
-
+  // finalized data object.
+  mostSearchedTerms: any = {
+    barChartType: 'bar',
+    barChartLabels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    barChartData: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    ],
+    barChartPlugins: [pluginDataLabels],
+    barChartLegend: true,
+    barChartOptions: {
+      responsive: true,
+      scales: { xAxes: [{}], yAxes: [{}] },
+      plugins: {
+        datalabels: {
+          anchor: 'end',
+          align: 'end',
+        }
+      }
+    }
+  }
 
   constructor(private globalSearchService: GlobalSearchService) { }
 
   ngOnInit() {
-    // this.loadChartingData();
+    this.loadChartingData();
   }
 
-  // loadChartingData = () => {
-  //   this.blockUI.start("Loading.......");
-  //   this.globalSearchService.getMostVisitedChartData().pipe(switchMap((visitedData: any) => {
-  //     if (visitedData) {
-  //       // set visited data
-  //       this.mostVisitedAnalysis = {
-  //         title: 'Most visited sites',
-  //         type: ChartType.BarChart,
-  //         data: visitedData.data,
-  //         columns: visitedData.columns,
-  //         options: {
-  //           animation: {
-  //             duration: 250,
-  //             easing: 'ease-in-out',
-  //             startup: true
-  //           }
-  //         }
-  //       }
+  loadChartingData = () => {
+    this.blockUI.start("Retreiving.......");
+    this.globalSearchService.getMostVisitedChartData().pipe(switchMap((visitedData: any) => {
+      if (visitedData) {
+        this.mostVisited.barChartLabels = visitedData.barChartLabels;
+        this.mostVisited.barChartData = visitedData.barChartData;
+      }
+      return this.globalSearchService.getMostSearchedKeyWords();
+    })).subscribe((keyWords: any) => {
+      if (keyWords) {
+        this.mostVisited.barChartLabels = keyWords.barChartLabels;
+        this.mostVisited.barChartData = keyWords.barChartData;
+      }
+      this.blockUI.stop();
+    }, error => {
+      this.blockUI.stop();
+      console.log(error);
+    })
+  }
 
-  //     }
-  //     return this.globalSearchService.getMostSearchedKeyWords();
-  //   })).subscribe((keyWords: any) => {
-  //     if (keyWords) {
+  loadChartData = () => {
 
-  //       // set searched keywords
-  //       this.mostSearchedKeyWord = {
-  //         title: 'Most searched keyword',
-  //         type: ChartType.BarChart,
-  //         data: keyWords.data,
-  //         columns: keyWords.columns,
-  //         options: {
-  //           animation: {
-  //             duration: 250,
-  //             easing: 'ease-in-out',
-  //             startup: true
-  //           }
-  //         }
-  //       }
-  //       this.blockUI.stop();
-  //     }
-  //   }, error => {
-  //     this.blockUI.stop();
-  //     console.log(error);
-  //   })
-  // }
+  }
 
   searchedTerm = (term: string) => {
     // searching part
-    this.blockUI.start("Loading.......");
+    this.blockUI.start("Retreiving.......");
     this.globalSearchService.searchTerm(term).subscribe((serviceResult: any) => {
       if (serviceResult) {
         this.searchedresults = serviceResult;
@@ -106,15 +103,4 @@ export class GlobalSearchComponent implements OnInit {
       this.blockUI.stop();
     })
   }
-
-  // @HostListener('window:resize', ['$event'])
-  // onResize(event: any) {
-  //   if (event.target.innerWidth > 1300) {
-  //     this.chartWidth = event.target.innerWidth / 2.2;
-  //     this.chartHieght = 420;
-  //   } else {
-  //     this.chartWidth = event.target.innerWidth / 1.8;
-  //     this.chartHieght = 420 / 2;
-  //   }
-  // }
 }
